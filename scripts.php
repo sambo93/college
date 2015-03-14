137ea0f741e0165a6eda0bc5f4b84d06c42ffe30
Sam <Sam@192.168.2.2>
2015-03-14 00:43:34 +0000

    Initial commit

--- scripts.php
+++ scripts.php
@@ -1,8 +1,2 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<script src="js/jquery-1.11.2.js"></script>
+<script src="js/bootstrap.js"></script>
