808378410668d806d462551287176af96a683c97
Sam <Sam@192.168.2.2>
2015-03-14 00:45:12 +0000

    Initial Commit

---
+++ mainMenu.php
@@ -0,0 +1,8 @@
+<nav class="navbar navbar-default">
+    <div class="container">
+        <ul class="nav navbar-nav">
+            <li><a href="viewScreens.php">Screens</a></li>
+            <li><a href="viewMovies.php">Movies</a></li>
+        </ul>
+    </div>
+</nav>
