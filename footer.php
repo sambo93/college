9d4ab5ac228f0a8659f927959d6cace3d2c0699c
Sam <Sam@192.168.2.2>
2015-03-15 03:37:08 +0000

    Initial Commit.

---
+++ footer.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-default navbar-fixed-bottom">
+    <div class="container">
+        <h5 class="navbar-text pull-left">&copy; Cinema, 2015</h5>
+    </div>
+</nav>
+<div class="modal fade" id="contact" role="dialog">
+    <div class="modal-dialog">
+        <form class="form-horizontal">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4>Contact Cinema</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="contact-name" class="col-md-2 control-label">Name</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" id="contact-name" placeholder="Full name" />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="contact-email" class="col-md-2 control-label">Email</label>
+                        <div class="col-md-10">
+                            <input type="email" class="form-control" id="contact-email" placeholder="you@example.com" />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="contact-message" class="col-md-2 control-label">Message</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" rows="8"></textarea>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <a class="btn btn-default" data-dismiss="modal">Close</a>
+                    <button class="btn btn-primary" type="submit">Send</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
