c791bd0b93cd95ae48ebef6dd68acff0c788ab03
Sam <Sam@192.168.2.5>
2015-04-01 17:25:29 +0100

    Initial Commit.

---
+++ createMovieForm.php
@@ -0,0 +1,116 @@
+<?php
+require_once 'Connection.php';
+require_once 'MovieTableGateway.php';
+
+$id = session_id();
+if ($id == "") {
+    session_start();
+}
+
+require 'ensureUserLoggedIn.php';
+
+$conn = Connection::getInstance();
+$movieGateway = new MovieTableGateway($conn);
+
+$movies = $movieGateway->getMovies();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <?php require "styles.php" ?>
+        <link rel="stylesheet" type="text/css" href=css/style.css>
+        <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+        <script type="text/javascript" src="js/movie.js"></script>
+    </head>
+    <body>
+        <?php require "toolbar.php" ?>
+        <?php require "header.php" ?>
+        <?php require "mainMenu.php" ?>
+        <div id="container">
+        <?php require 'toolbar.php' ?>
+        <h1>Create Movie Information Form</h1>
+        <?php
+        if (isset($errorMessage)) {
+            echo '<p>Error: ' . $errorMessage . '</p>';
+        }
+        ?>
+        <form id="createMovieForm"
+              name="createMovieForm"
+              action="createMovie.php" 
+              method="POST">
+            <table border="0">
+                <tbody>
+                    <tr>
+                        <td>Title</td>
+                        <td>
+                            <input type="text" name="title" value="<?php
+                                    if (isset($_POST) && isset($_POST['title'])) {
+                                        echo $_POST['title'];
+                                    }
+                                ?>" />
+                            <span id="titleError" class="error"></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Year Of Release</td>
+                        <td>
+                            <input type="text" name="yearOfRelease" value="<?php
+                                    if (isset($_POST) && isset($_POST['yearOfRelease'])) {
+                                        echo $_POST['yearOfRelease'];
+                                    }
+                                ?>" />
+                            <span id="yearOfReleaseError" class="error"></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Running Time</td>
+                        <td>
+                            <input type="text" name="runningTime" value="<?php
+                                    if (isset($_POST) && isset($_POST['runningTime'])) {
+                                        echo $_POST['runningTime'];
+                                    }
+                                ?>" />
+                            <span id="runningTimeError" class="error"></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Age Classification</td>
+                        <td>
+                            <input type="text" name="ageClassification" value="<?php
+                                    if (isset($_POST) && isset($_POST['ageClassification'])) {
+                                        echo $_POST['ageClassification'];
+                                    }
+                                ?>" />
+                            <span id="ageClassificationError" class="error"></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Director</td>
+                        <td>
+                            <input type="text" name="director" value="<?php
+                                    if (isset($_POST) && isset($_POST['director'])) {
+                                        echo $_POST['director'];
+                                    }
+                                ?>" />
+                            <span id="directorError" class="error"></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <input type="submit" value="Create Movie" name="createMovie" />
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </form>
+        </div>
+        <?php require "footer.php" ?>
+        <?php require "scripts.php" ?>
+    </body>
+</html>
+
+
